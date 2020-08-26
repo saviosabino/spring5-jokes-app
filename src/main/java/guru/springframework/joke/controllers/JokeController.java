30cb054882d7433b4bd35bff1a38c00aa1653bd3
savio_pcdvwdw <savio_pcdvwdw@LAPTOP-3PPP5OR1>
2020-08-25 22:52:34 -0300

    joke view added, runned, all fine. lecture 59

--- src/main/java/guru/springframework/joke/controllers/JokeController.java
+++ src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -1,10 +1,13 @@
 package guru.springframework.joke.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.joke.services.JokeService;
 
+@Controller
 public class JokeController {
 
 	private JokeService jokeservice;
@@ -14,6 +17,7 @@ public class JokeController {
 		this.jokeservice = jokeservice;
 	}
 	
+	@RequestMapping({"/", ""})
 	public String showJoke(Model model) {
 		model.addAttribute("joke", jokeservice.getJoke());
 		return "chucknorris";
